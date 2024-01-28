382e95d00e5a0e3b7a39bc4b92980b73bf290bf8
LinMihigo <linmihigo@gmail.com>
2024-01-28 21:34:02 +0200

    Chore: added links to content

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -16,12 +16,14 @@ interface bookCardProp {
         key: string;
         title: string;
         author_name: string[];
+        author_key: string
         first_publish_year: number;
         cover_i: string;
         ebook_count_i: number;
         edition_count: number
         language: string[];
-        edition_key: string[]
+        edition_key: string[];
+        cover_edition_key: string;
     }[];
     isLoaded: boolean;
     isLoading: boolean;
@@ -36,32 +38,37 @@ export default function BookCard({ bookData, isLoaded, isLoading }: bookCardProp
 
                 <Card key={res.key} className={cn("flex w-[800px] min-h-[190px] bg-stone-50 mb-2 mx-auto")}>
                     <div className='min-w-[103px] min-h-[164px] w-[103px] h-[164px] my-auto ml-4'>
-                        <img className='w-full h-full object-fit rounded'
-                            src={`https://covers.openlibrary.org/b/id/${res.cover_i}-L.jpg`}
-                            alt={`Cover of ${res.title}`}
-                            onLoad={(e: React.SyntheticEvent<HTMLImageElement>) => {
-                                const img = e.target as HTMLImageElement
-                                if (img.naturalHeight < 5) {
-                                    img.src = 'https://placehold.jp/50/3d4070/ffffff/1030x1640.png?text=No%20img%20found'
-                                }
-                            }}
-                        />
+                        <a href={`https://openlibrary.org${res.key}?edition=key%3A/books/${res.cover_edition_key}`} target="_blank">
+                            <img className='w-full h-full object-fit rounded'
+                                src={`https://covers.openlibrary.org/b/id/${res.cover_i}-L.jpg`}
+                                alt={`Cover of ${res.title}`}
+                                onLoad={(e: React.SyntheticEvent<HTMLImageElement>) => {
+                                    const img = e.target as HTMLImageElement
+                                    if (img.naturalHeight < 5) {
+                                        img.src = 'https://placehold.jp/50/3d4070/ffffff/1030x1640.png?text=No%20img%20found'
+                                    }
+                                }}
+                            />
+                        </a>
                     </div>
                     <div className='min-w-[465px] my-auto ml-4 mr-4'>
                         <CardHeader className='p-0 pb-2'>
-                            <CardTitle>{res.title.length > 40 ? `${res.title.substring(0, 35)}...` : res.title}</CardTitle>
+                            <a href={`https://openlibrary.org${res.key}?edition=key%3A/books/${res.cover_edition_key}`} target="_blank">
+                                <CardTitle>{res.title.length > 40 ? `${res.title.substring(0, 35)}...` : res.title}</CardTitle>
+                            </a>
+
                             <CardDescription className='text-md text-stone-700 dark:text-stone-300'>By
                                 {" "}
                                 {res.author_name ?
                                     res.author_name.slice(0, 3).map((name: string, i: number) => {
                                         if (i < res.author_name.length - 1 && name.length < 50 && res.author_name.length > 1) {
-                                            return name + ", "
+                                            return <a key={i} href={`https://openlibrary.org/authors/${res.author_key[i]}`} target='_blank'>{name + ", "}</a>
                                         } else if (name.length > 50) {
-                                            return `${name.substring(0, 50)}...`
+                                            return <a key={i} href={`https://openlibrary.org/authors/${res.author_key[i]}`} target='_blank'>{`${name.substring(0, 50)}...`}</a>
                                         } else if (i === res.author_name.length - 1 && res.author_name.length !== 1) {
-                                            return "and " + name
+                                            return <a key={i} href={`https://openlibrary.org/authors/${res.author_key[i]}`} target='_blank'>{"and " + name}</a>
                                         } else {
-                                            return name
+                                            return <a key={i} href={`https://openlibrary.org/authors/${res.author_key[i]}`} target='_blank'>{name}</a>
                                         }
                                     }) :
                                     "Couldn't fetch author name(s)"
@@ -70,8 +77,10 @@ export default function BookCard({ bookData, isLoaded, isLoading }: bookCardProp
                         </CardHeader>
                         <CardContent className="text-sm text-stone-500 dark:text-stone-400 p-0 ml-0">
                             <p>First published in {res.first_publish_year}</p>
-                            <p>
-                                {res.edition_count === 1 ? `${res.edition_count} edition` : `${res.edition_count} editions`},
+                            <p className='text-stone-600'>
+                                <a className='text-stone-900' href={`https://openlibrary.org${res.key}?edition=key%3A/books/${res.cover_edition_key}&mode=all#editions-list`} target="_blank">
+                                    {res.edition_count === 1 ? `${res.edition_count} edition` : `${res.edition_count} editions`}
+                                </a>,
                                 {" "}
                                 {res.ebook_count_i === 1 ? `${res.ebook_count_i} e-book` : `${res.ebook_count_i} e-books`}
                                 {
@@ -91,7 +100,7 @@ export default function BookCard({ bookData, isLoaded, isLoading }: bookCardProp
                         </CardContent>
                     </div>
                     <CardFooter className='flex flex-col gap-2 justify-center p-4'>
-                        <Button className="w-[150px]">View Details</Button>
+                        <Button className="w-[150px]">Preview</Button>
                         <Button variant='outline' className="w-[150px]">Read</Button>
                     </CardFooter>
                 </Card>
