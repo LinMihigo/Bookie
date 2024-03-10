86bb75894be0bc98a3d3119d0c994b9a885a3137
LinMihigo <linmihigo@gmail.com>
2024-03-10 16:39:43 +0200

    Chore: added text to the landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-
+import { Button } from "./components/ui/button"
 import { Link } from "react-router-dom"
 function App() {
 
   return (
-    <div>
-      <Link to='/Search'>Search</Link>
+    <div className="flex flex-col gap-2 justify-center items-center w-screen h-screen">
+      <p>This landing page is for routing testing and still under development,</p>
+      <p>To search for books, click the button below.</p>
+      <Link to='/Search'>
+        <Button>Search</Button>
+      </Link>
     </div>
   )
 }
